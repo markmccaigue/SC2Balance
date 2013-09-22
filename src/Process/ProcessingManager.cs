95e7a1c62dbe30cd33ff031c62a4136d936ce4a4
sc2balance <sc2balance@gmail.com>
2013-09-22 17:49:27 +0100

    Improving Processing Performance
    Some general tuning of the processing code, avoiding any architectural
    changes such as batching etc. at this point.
    
    Changes include:
    -Determining winner from Player1's matches (max 25 records) instead of
    all known matches (max 15000 records).
    -Breaking some multiple-expression FirstOrDefault clauses into multiple
    smaller queries.
    -Removing in-memory list of uniqueMatches in favour of just using the
    one on the data context, this is simpler and a little faster.
    -Removing redundant ToList().
    
    Resultant performance gains with sample data are measured at just over
    50%.

--- src/Process/ProcessingManager.cs
+++ src/Process/ProcessingManager.cs
@@ -24,7 +24,6 @@ namespace Sc2Balance.Process
             return db.Ingestions.OrderByDescending(x => x.Id).First().Id;
         }
 
-        // TODO: Performance...
         public void RunUniqueGamesPass(int ingestionId = 0)
         {
             using (var db = new DataContext())
@@ -32,33 +31,30 @@ namespace Sc2Balance.Process
                 var processingRun = new ProcessingRun {DateTime = DateTime.UtcNow};
                 ingestionId = ingestionId == 0 ? GetLatestIngestionId(db) : ingestionId;
 
-                var latestMatches = db.Matches.Include(x => x.LadderMember).Where(x => x.Ingestion.Id == ingestionId);
+                var latestMatches = db.Matches.Where(x => x.Ingestion.Id == ingestionId);
                 var duplicateMatches = latestMatches.Where(x => latestMatches.Any(y => y.Date == x.Date && x.Id != y.Id));
 
                 // Avoid access to modified closure
                 var matches = duplicateMatches;
-                var badMatches = duplicateMatches.Where(x => matches.FirstOrDefault(d => d.Date == x.Date && d.Decision == "WIN") ==
-                                            null);
+                var badMatches = duplicateMatches.Where(x => matches.Where(d => d.Date == x.Date).FirstOrDefault(d => d.Decision == "WIN") == null);
                 duplicateMatches = duplicateMatches.Except(badMatches);
 
-                var populatedMatches = duplicateMatches.Select(x => new
+                var populatedMatches = duplicateMatches.Include(x => x.LadderMember).Select(x => new
                     {
                         Date = x.Date,
                         LadderMember1 = x.LadderMember,
-                        LadderMember2 = duplicateMatches.FirstOrDefault(y => x.Date == y.Date && x.Id != y.Id).LadderMember,
+                        LadderMember2 = duplicateMatches.Where(y => x.Date == y.Date).FirstOrDefault(y => x.Id != y.Id).LadderMember,
                         Map = x.Map,
                         Speed = x.Speed,
-                        Type = x.Type,
-                        Winner = duplicateMatches.FirstOrDefault(d => d.Date == x.Date && d.Decision == "WIN").LadderMember
+                        Type = x.Type
                     }
-                ).ToList();
+                );
 
                 var ingestion = db.Ingestions.FirstOrDefault(i => i.Id == ingestionId);
-                var uniqueMatches = new List<UniqueGmMatch>();
                 foreach (var populatedMatch in populatedMatches)
                 {
                     var date = UnixTimeStampToDateTime(populatedMatch.Date);
-                    if (uniqueMatches.Any(u => DateTime.Equals(date, u.DateTime)))
+                    if (db.UniqueGmMatches.Any(u => DateTime.Equals(date, u.DateTime)))
                     {
                         continue;
                     }
@@ -72,14 +68,12 @@ namespace Sc2Balance.Process
                         Map = populatedMatch.Map,
                         Speed = populatedMatch.Speed,
                         Type = populatedMatch.Type,
-                        Winner = populatedMatch.Winner,
+                        Winner = populatedMatch.LadderMember1.Matches.Where(x => x.Date == populatedMatch.Date).First().Decision == "WIN" ? populatedMatch.LadderMember1 : populatedMatch.LadderMember2,
                         ProcessingRun = processingRun
                     };
-
-                    uniqueMatches.Add(hydratedMatch);
+                    db.UniqueGmMatches.Add(hydratedMatch);
                 }
 
-                uniqueMatches.ForEach(u => db.UniqueGmMatches.Add(u));
                 db.SaveChanges();
             }
         }
