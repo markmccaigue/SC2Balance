f223aae7c84a32e48175cc99b81a281bb51fe065
sc2balance <sc2balance@gmail.com>
2013-10-05 01:18:44 +0100

    Adding backend logging
    Some basic logging, closes #12

---
+++ src/IngestAndProcessRunner/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC2Balance.IngestAndProcessRunner
+{
+    public static class Logger
+    {
+        private static void Log(string message)
+        {
+            var path = string.Format("{0}{1}{2}{3}", System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Path.DirectorySeparatorChar, DateTime.UtcNow.ToString("ddMMyyHHmmss"), ".txt");
+            File.WriteAllText(path, message);
+        }
+
+        public static void LogError(Exception exception)
+        {
+            var message = String.Format("{0}{1}{2}{3}{4}{5}", exception.Message, Environment.NewLine, exception.Source, Environment.NewLine, exception.StackTrace, Environment.NewLine);
+            Log(message);
+        }
+    }
+}
---
+++ src/IngestAndProcessRunner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SC2Balance.Ingest;
+using Sc2Balance.Process;
+
+namespace SC2Balance.IngestAndProcessRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new IngestionManager().RunNewIngestion();
+                new ProcessingManager().RunUniqueGamesPass();
+            }
+            catch (Exception exception)
+            {
+                Logger.LogError(exception);
+                System.Environment.Exit(exception.HResult);
+            }
+        }
+    }
+}
